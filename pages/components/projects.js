ecb0f1508f9e11e281481351a6bdabfb8b740e9c
Abdul De Borja <86055842+AbdulDeBorja09@users.noreply.github.com>
2024-07-12 22:19:00 +0800

    update projects

--- pages/components/projects.js
+++ pages/components/projects.js
@@ -40,6 +40,17 @@ export default function Projects() {
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum dicta officia illo ipsa reiciendis! Consequatur autem dolore alias perferendis reprehenderit corrupti nihil</p>
                 <a>VIEW PROJECT <span className="right-arrow">&#8594;</span></a>
             </div>
+            <div className="col-lg-4">
+                <div className="tags d-flex">
+                    <span>E-COMMERECE</span>
+                    <span>WEBSITE</span>
+                    <span>NEXT JS</span>
+                </div>
+                <img className="project-img" src="/image/aboutbg.png"></img>
+                <h2>ISTORIA COFFEE SHOP</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum dicta officia illo ipsa reiciendis! Consequatur autem dolore alias perferendis reprehenderit corrupti nihil</p>
+                <a>VIEW PROJECT <span className="right-arrow">&#8594;</span></a>
+            </div>
 
 
         </div>
